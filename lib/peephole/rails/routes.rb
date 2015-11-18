f42c23196b202cf1922167b7a0f8fd4407b8d299
Jack Jennings <jack@remote.gd>
2015-11-17 16:00:01 -0800

    Add arguments to routing helpers

--- lib/peephole/rails/routes.rb
+++ lib/peephole/rails/routes.rb
@@ -1,15 +1,17 @@
 module ActionDispatch::Routing
   class Mapper
 
-    def with_tenant
-      scope constraints: Peephole::TenantSubdomainConstraint.new do
+    def with_tenant *args
+      constraints = Peephole::TenantSubdomainConstraint.new *args
+      scope constraints: constraints do
         yield if block_given?
       end
     end
     alias_method :for_tenant, :with_tenant
 
-    def without_tenant
-      scope constraints: Peephole::WithoutSubdomainConstraint.new do
+    def without_tenant *args
+      constraints = Peephole::WithoutSubdomainConstraint.new *args
+      scope constraints: constraints do
         yield if block_given?
       end
     end
